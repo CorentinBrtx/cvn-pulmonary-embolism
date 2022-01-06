d9d5cd1ba76467363cf7e6b508e89b3bcf6cb736
Corentin Berteaux <corentin.berteaux98@gmail.com>
2022-01-06 12:50:36 +0100

    Add nnunet scripts

---
+++ slurm_scripts/nnUNet/nnUNet_cadpe_preprocess.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=nnunet_cadpe_preprocess
+#SBATCH --output=%x.o%j
+#SBATCH --time=3:00:00
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=32G
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=8
+
+# Load necessary modules
+module purge
+module load anaconda3/2021.05/gcc-9.2.0
+module load cuda/10.2.89/intel-19.0.3.199
+
+# Activate anaconda environment
+source activate pulmembol
+
+# Run nnUnet commands
+#nnUNet_plan_and_preprocess -t 501 --verify_dataset_integrity
+nnUNet_plan_and_preprocess -t 501 
