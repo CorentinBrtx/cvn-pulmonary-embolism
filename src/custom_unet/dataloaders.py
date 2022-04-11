0826f4f57cb41dec22c88fccd9b7c48a73c8945d
Corentin Berteaux <corentin.berteaux98@gmail.com>
2022-04-11 16:01:12 +0200

    Add patches for the images
    Co-authored-by: Castavo <Castavo@users.noreply.github.com>

--- src/custom_unet/dataloaders.py
+++ src/custom_unet/dataloaders.py
@@ -1,15 +1,19 @@
-from torchvision import transforms
+import os
+
+import nibabel as nib
+import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset, random_split
-import os
-import numpy as np
-import nibabel as nib
+from torchvision import transforms
 
 
 class CadpeDataset(Dataset):
-    def __init__(self, img_dir, seg_dir, transform=None, with_frangi=False, n_patches=None):
+    def __init__(
+        self, img_dir, seg_dir, transform=None, with_frangi=False, patch_size=(50, 50, 50)
+    ):
         self.transform = transform
-        self.n_patches = n_patches or (1, 1, 1)
+        self.patch_size = patch_size
+        self.patch_positions = self.compute_patch_positions()
 
         self.imgs_paths = [
             os.path.join(img_dir, img_name)
@@ -32,18 +36,41 @@ class CadpeDataset(Dataset):
                 if seg_name.endswith(".nii.gz")
             ].sort()
 
+    def compute_patch_positions(self):
+        patch_positions = []
+        for i, img_path in enumerate(self.imgs_paths):
+            img = nib.load(img_path)
+            img_shape = img.get_data_shape()
+            patch_positions.extend(
+                [
+                    (i, (a, b, c))
+                    for a in range(img_shape[0] // self.patch_size[0] - 1)
+                    for b in range(img_shape[1] // self.patch_size[1] - 1)
+                    for c in range(img_shape[2] // self.patch_size[2] - 1)
+                ]
+            )
+
+        return patch_positions
+
     def __len__(self):
         return len(self.imgs_paths)
 
-    def __getitem__(self, idx):
-        image = nib.load(self.imgs_paths[idx]).get_data().astype(np.float32)
-        seg = nib.load(self.segs_paths[idx]).get_data().astype(np.float32)
+    def __getitem__(self, patch_idx):
+        img_idx, patch_pos = self.patch_positions[patch_idx]
+        patch_slice = np.s_[
+            patch_pos[0] : patch_pos[0] + self.patch_size[0],
+            patch_pos[1] : patch_pos[1] + self.patch_size[1],
+            patch_pos[2] : patch_pos[2] + self.patch_size[2],
+        ]
+        image = nib.load(self.imgs_paths[img_idx]).get_data().astype(np.float32)[patch_slice]
+        seg = nib.load(self.segs_paths[img_idx]).get_data().astype(np.float32)[patch_slice]
         if self.transform:
             image = self.transform(image)
-            image = image[np.newaxis]
         if self.frangi_paths is not None:
-            frangi = nib.load(self.frangi_paths[idx]).get_data().astype(np.float32)
+            frangi = nib.load(self.frangi_paths[img_idx]).get_data().astype(np.float32)[patch_slice]
             image = np.array([image, frangi])
+        else:
+            image = image[np.newaxis]
         return image, seg
 
 
@@ -59,13 +86,13 @@ def get_data_loaders(
     """Get the data loaders"""
     train_transform = transforms.Compose(
         [
-            transforms.ToTensor(), 
+            transforms.ToTensor(),
             # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ]
     )
     test_transform = transforms.Compose(
         [
-            transforms.ToTensor(), 
+            transforms.ToTensor(),
             # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
         ]
     )
@@ -88,7 +115,6 @@ def get_data_loaders(
         test_loader = DataLoader(dataset=test_dataset, shuffle=False)
     else:
         test_loader = None
-
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(dataset=val_dataset, shuffle=False)
